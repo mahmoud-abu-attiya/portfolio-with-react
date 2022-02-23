88270a7f1cd4aff64d99126b4b2745ab311d09d4
mahmoud <mahmoud.abuattiy106@gmail.com>
2022-02-23 05:14:32 +0200

    fontawsome

--- src/Conponents/Footer/index.js
+++ src/Conponents/Footer/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faCopyright} from '@fortawesome/free-solid-svg-icons'
-// import "bootstrap-icons/font/bootstrap-icons.css";
 import './style.scss';
 
 const Footer = ()=> {
@@ -10,7 +7,7 @@ const Footer = ()=> {
         <div className="container">
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            {/* <FontAwesomeIcon icon={faCopyright} /> */}
+            <i className="fas fa-copyright"></i>
             </a>
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li><a href="/" className="nav-link px-2 text-secondary">Home</a></li>
