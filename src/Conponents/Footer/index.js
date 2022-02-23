2f9eb9bf913f5dbd58bfc72cabc332f9070b2f24
mahmoud <mahmoud.abuattiy106@gmail.com>
2022-02-23 18:19:45 +0200

    last

--- src/Conponents/Footer/index.js
+++ src/Conponents/Footer/index.js
@@ -8,6 +8,7 @@ const Footer = ()=> {
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
             <i className="fas fa-copyright"></i>
+            {new Date().getFullYear()}
             </a>
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li><a href="/" className="nav-link px-2 text-secondary">Home</a></li>
